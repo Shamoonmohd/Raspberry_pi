32c0efff1a374cda50aa70c6710c681fd57f7e47
Shamoonmohd <Shamoonmohd>
2019-05-28 15:04:03 +0530

    Code for Flame detector sensor

---
+++ Flame_sensor.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+
+Flame_sensor_pin = 11
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(Flame_sensor_pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+    
+def loop():
+    while(True):
+        if GPIO.input(Flame_sensor_pin)== GPIO.HIGH:
+            print("Flame detected")
+        
+def destroy():
+    GPIO.cleanup()
+
+if __name__=="__main__":
+    setup()
+    try:
+        loop()
+    except:
+        destroy()
